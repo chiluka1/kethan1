89e6a5e7f7144f77871bd39444f4d1fe71e33a54
flipkart <kethansagar1998@gmail.com>
2022-06-13 15:21:13 +0530

    conditional styling and mapping through array

--- src/App.js
+++ src/App.js
@@ -5,12 +5,36 @@ import './App.css';
 function App() {
   let name = "kethan";
   var num = "123456789";
+  let blueColor = false;
+  let safeColor = false;
+  let arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 13, 234, 46, 57,];
+  let strArray = ["giri", "suresh", "srimani", "dileep", "lalith", "vamshi"]; 
+  let arrObj = [
+    { Name : "kethan" , Role : "analyst"},
+    {Name : "giri" , Role : "associate"},
+    {Name : "srimani" , Role : "engineer"},
+   ];
   return (
     <div className="App"> 
-      <h1>heading!!!!!</h1>
+      <h1 className = { blueColor ? "header" : safeColor ? "safe" : "error" }>Heading</h1>
       <p>{name}</p>
       <p>{num}</p>
-      { "  " ? <h1> {name}</h1> : <h1>no number</h1>};
+      {true ? <h1> {name}</h1> : <h1>{num}</h1>}
+      { arr.map[(index, value) => {
+       return <h4>{value}</h4>
+      }]}
+      { strArray.map[(value,index) => {
+        return <h1>{value}</h1>
+      }]}
+
+      { arrObj.map[(value, index) => {
+        return 
+        <div className = "user-item">
+        <h1>Name : {value.name}</h1> 
+        <h1>Name : {value.role}</h1>
+        </div>
+      }]}
+ 
     </div>
       
   );
